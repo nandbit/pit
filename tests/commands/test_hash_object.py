0edf63b998ef0e29fc45a1048370137fac9d9b8a
nandbit <nikitagalayda@gmail.com>
2026-03-24 11:41:57 +0800

    add hash-object file test

--- tests/commands/test_hash_object.py
+++ tests/commands/test_hash_object.py
@@ -1,8 +1,23 @@
+import os
+
 from pit.commands import HashObjectCommand, HashObjectCommandArgs
 
 
 def test_hash_string():
     args = HashObjectCommandArgs("test content", stdin=True)
+    command = HashObjectCommand(args)
+    result = command.execute()
+    correct_result = "08cf6101416f0ce0dda3c80e627f333854c4085c"
+
+    assert result == correct_result
+
+
+def test_hash_file(tmp_path):
+    filepath = os.path.join(tmp_path, "test.txt")
+    with open(filepath, "w") as f:
+        f.write("test content")
+
+    args = HashObjectCommandArgs(filepath, stdin=False)
     command = HashObjectCommand(args)
     result = command.execute()
     correct_result = "08cf6101416f0ce0dda3c80e627f333854c4085c"
