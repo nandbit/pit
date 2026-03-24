1e4792bcd6b4d506183ba1f5339e6282e50c686b
nandbit <nikitagalayda@gmail.com>
2026-03-24 17:51:49 +0800

    add hash-object create file test

--- tests/commands/test_hash_object.py
+++ tests/commands/test_hash_object.py
@@ -1,10 +1,25 @@
 import os
 
+import pytest
+
 from pit.commands import HashObjectCommand, HashObjectCommandArgs
 
 
+@pytest.fixture()
+def empty_objects_dir(tmp_path):
+    repo = tmp_path / ".pit" / "objects"
+    repo.mkdir(parents=True, exist_ok=True)
+
+    return repo
+
+
 def test_hash_string():
-    args = HashObjectCommandArgs("test content", stdin=True)
+    args = HashObjectCommandArgs(
+        target="test content",
+        write=False,
+        stdin=True,
+        content_type="blob",
+    )
     command = HashObjectCommand(args)
     result = command.execute()
     correct_result = "08cf6101416f0ce0dda3c80e627f333854c4085c"
@@ -17,9 +32,35 @@ def test_hash_file(tmp_path):
     with open(filepath, "w") as f:
         f.write("test content")
 
-    args = HashObjectCommandArgs(filepath, stdin=False)
+    args = HashObjectCommandArgs(
+        target=filepath,
+        write=False,
+        stdin=False,
+        content_type="blob",
+    )
     command = HashObjectCommand(args)
     result = command.execute()
     correct_result = "08cf6101416f0ce0dda3c80e627f333854c4085c"
 
     assert result == correct_result
+
+
+def test_hash_string_write(empty_objects_dir):
+    args = HashObjectCommandArgs(
+        target="test content",
+        write=True,
+        stdin=True,
+        content_type="blob",
+    )
+    command = HashObjectCommand(args)
+    result = command.execute()
+    correct_result = "08cf6101416f0ce0dda3c80e627f333854c4085c"
+
+    created_file_path = os.join(
+        empty_objects_dir,
+        "08/cf6101416f0ce0dda3c80e627f333854c4085c",
+    )
+
+    assert result == correct_result
+    assert os.path.exists(created_file_path)
+    assert os.path.isfile(created_file_path)
